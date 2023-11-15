23469934264a04d0af9c87b84f464292643c54f8
Azlan Hasan <Azlan.Hasan@xfab.com>
2023-11-15 13:43:06 +0800

    Update

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ $(function () {
 
   let currentZoom = 1;
 
-    $('#zoomIn').click(function() {
-        currentZoom += 0.1;
-        $('#box-container').css('transform', 'scale(' + currentZoom + ')');
-    });
+  $('#zoomIn').click(function () {
+    currentZoom += 0.1;
+    $('#box-container').css('transform', 'scale(' + currentZoom + ')');
+  });
 
-    $('#zoomOut').click(function() {
-        currentZoom = Math.max(0.1, currentZoom - 0.1);
-        $('#box-container').css('transform', 'scale(' + currentZoom + ')');
-    });
-    
+  $('#zoomOut').click(function () {
+    currentZoom = Math.max(0.1, currentZoom - 0.1);
+    $('#box-container').css('transform', 'scale(' + currentZoom + ')');
+  });
+
+  $('#zoomReset').click(function () {
+    currentZoom = 1; // Reset the zoom level to the default
+    $('#box-container').css('transform', 'scale(' + currentZoom + ')');
+  });
+
   $('#box-container').on('click', '.resizable-box', function () {
     $('#boxWidth').val($(this).width());
     $('#boxHeight').val($(this).height());
@@ -20,6 +25,7 @@ $(function () {
     $('#boxGroupId').val($(this).data('group-id'));
     $('#boxLabel').val($(this).find('.box-label').text()); // Input for editing label name
     $('#iconOnlyCheck').prop('checked', $(this).hasClass('icon-only')); // Checkbox for icon only
+    $('#boxRemarks').val($(this).data('remarks')); // Input for editing remarks
 
     var icon = $(this).find('i');
     if (icon.length > 0) {
@@ -32,6 +38,24 @@ $(function () {
     $('#boxModal').modal('show');
   });
 
+  // Event listener for the delete button in the modal
+  $('#deleteBox').click(function () {
+    // Get the current box ID that we are editing
+    var currentBoxId = $('#confirmBox').data('currentBoxId');
+
+    if (currentBoxId) {
+      // Remove the box with the current ID from the DOM
+      $('#' + currentBoxId).remove();
+
+      // Hide the modal after deletion
+      $('#boxModal').modal('hide');
+      // Remove the stored current box ID
+      $('#confirmBox').removeData('currentBoxId');
+    } else {
+      alert('No box is selected for deletion!');
+    }
+  });
+
   function randomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -41,24 +65,44 @@ $(function () {
     return color;
   }
 
-  function createBox(width, height, shapeType, color, groupId, iconClass, iconOnly = false, label = '') {
-    const gap = 1; // Assuming a gap for aesthetics
-    const boxNumber = ++maxZIndex; // Increment the zIndex for each new box
-    const rows = Math.floor((boxNumber - 1) / 5);
-    const cols = (boxNumber - 1) % 5;
-  
-    const topPosition = rows * (height + gap);
-    const leftPosition = cols * (width + gap);
-  
+  function createBox(width, height, shapeType, color, groupId, iconClass, iconOnly = false, label = '', columns = 10, remarks = '') {
+    const gap = 1; // Gap for aesthetics
+    const boxNumber = ++maxZIndex; // Increment zIndex for each new box
+
+    // Use the columns parameter to determine rows and cols
+    const rows = Math.floor((boxNumber - 1) / columns);
+    const cols = (boxNumber - 1) % columns;
+
+    // Log total number of boxes
+    const totalBoxes = $('.resizable-box').length;
+    console.log('Total number of boxes:', totalBoxes % columns);
+
+    const lastBox = $('.resizable-box').last();
+    let lastTop = lastBox.length ? parseInt(lastBox.css('top'), 10) : 0;
+    let lastLeft = lastBox.length ? parseInt(lastBox.css('left'), 10) : 0;
+
+    console.log(lastTop);
+
+    if (totalBoxes > 0 && totalBoxes % columns === 0) {
+      lastTop += height + gap;
+      lastLeft = 0;
+    } else {
+      lastLeft += lastBox.length ? width + gap : 0;
+    }
+    // Calculate positions, considering the width, height, and gap
+    const topPosition = lastTop;
+    const leftPosition = lastLeft;
+
     const boxLabel = label || 'Box ' + boxNumber;
     const circleClass = shapeType === 'circle' ? 'circle-shape' : '';
-  
+
     const box = $('<div/>', {
       id: 'box' + boxNumber,
       class: 'resizable-box ' + circleClass + (iconOnly ? ' icon-only' : ''),
       'data-box_number': boxNumber,
       'data-group-id': groupId,
       'data-shape-type': shapeType,
+      'data-remarks': remarks,
       css: {
         position: 'absolute',
         top: topPosition + 'px',
@@ -69,30 +113,32 @@ $(function () {
         zIndex: boxNumber
       }
     });
-  
+
     if (iconOnly && iconClass) {
       box.append($('<i/>', { class: 'fa ' + iconClass }));
       box.css({
         'background-color': 'transparent',
         'border': 'none'
       });
-    } else {
+    }
+    else {
       if (iconClass) {
         box.append($('<i/>', { class: 'fa ' + iconClass + ' icon' }));
       }
       box.append($('<span/>', { class: 'box-label', text: boxLabel }));
     }
-  
+    //box.data('remarks', remarks);
+
     box.appendTo('#box-container').draggable({ containment: "#container" }).resizable({
       // Optional: If you want to maintain aspect ratio while resizing, uncomment below
       // aspectRatio: true,
       // Optional: If you want to set the containment within the parent, uncomment below
       // containment: "parent"
     });
-  
+
     return box;
   }
-  
+
 
   function rgb2hex(rgb) {
     if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
@@ -129,14 +175,43 @@ $(function () {
     });
   }
 
+  function toggleDragResize(enable) {
+    if (enable) {
+      $('.resizable-box').draggable('enable').resizable('enable');
+    } else {
+      $('.resizable-box').draggable('disable').resizable('disable');
+    }
+  }
 
+  toggleDragResize($('#editModeCheck').is(':checked'));
 
+  // Event handler for the EditMode checkbox change
+  $('#editModeCheck').change(function () {
+    var isInEditMode = $(this).is(':checked');
+    toggleDragResize(isInEditMode);
+  });
+
+  // Event listener for the "Create 100" button
+  $('#create100').click(function () {
+    const numberOfBoxes = 200;
+    const boxWidth = 100; // Default width
+    const boxHeight = 100; // Default height
+    const shapeType = 'box'; // Default shapeType
+    const groupId = 'default-group'; // Default groupId
+    const columns = 20; // The dynamic number of columns you want
+
+    for (let i = 0; i < numberOfBoxes; i++) {
+      const color = '#fff'; // Generate a random color for each box
+      const label = 'Box ' + (maxZIndex + 1); // Label for each box
+      createBox(boxWidth, boxHeight, shapeType, color, groupId, '', false, label, columns);
+    }
+  });
 
   $('#create').click(function () {
     $('#boxModal').modal('show');
   });
 
-  $('#confirmBox').click(function() {
+  $('#confirmBox').click(function () {
     var currentBoxId = $(this).data('currentBoxId');
     var width = $('#boxWidth').val();
     var height = $('#boxHeight').val();
@@ -145,11 +220,14 @@ $(function () {
     var iconClass = $('#boxIcon').val();
     var iconOnly = $('#iconOnlyCheck').is(':checked');
     var label = $('#boxLabel').val();
+    var remarks = $('#boxRemarks').val(); // Get the remarks from the input field
 
     if (currentBoxId) {
       var box = $('#' + currentBoxId);
       box.empty(); // Clear any previous content
       box.removeClass('icon-only');
+      box.data('remarks', remarks); // Save the remarks to the box's data attributes
+
       if (iconOnly) {
         box.addClass('icon-only');
         box.append($('<i/>', { class: 'fa ' + iconClass }));
@@ -157,7 +235,8 @@ $(function () {
           'background-color': 'transparent',
           'border': 'none'
         }).data('group-id', groupId);
-      } else {
+      }
+      else {
         if (iconClass) {
           box.prepend($('<i/>', { class: 'fa ' + iconClass }));
         }
@@ -171,9 +250,11 @@ $(function () {
         'width': width + 'px',
         'height': height + 'px'
       });
-    } else {
+    }
+    else {
       // Create a new box
-      createBox(parseInt(width), parseInt(height), 'box', color, groupId, iconClass, iconOnly, label);
+      createBox(parseInt(width), parseInt(height), 'box', color, groupId, iconClass, iconOnly, label, 1, remarks);
+
     }
 
     $('#boxModal').modal('hide');
@@ -186,7 +267,7 @@ $(function () {
     const height = $(this).data('h');
     const shapeType = $(this).data('shape-type'); // Use hyphenated data attributes
 
-    createBox(width, height, shapeType,randomColor());
+    createBox(width, height, shapeType, randomColor());
   });
 
 
@@ -198,9 +279,11 @@ $(function () {
         width: $(this).css('width'),
         height: $(this).css('height'),
         zIndex: parseInt($(this).css('z-index')),
-        label: $(this).text(),
+        label: $(this).find('.box-label').text(),
         color: $(this).css('background-color'),
-        shapeType: $(this).attr('shape-type') // Use the hyphenated attribute here as well
+        shapeType: $(this).attr('data-shape-type'), // Use the 'data-shape-type' attribute
+        groupId: $(this).data('group-id'), // Use the 'data-group-id' attribute
+        remarks: $(this).data('remarks') // Retrieve the 'data-remarks' attribute
       };
     }).get();
 
@@ -210,6 +293,7 @@ $(function () {
 
     $('#json-output').text(JSON.stringify(jsonOutput, null, 2));
   });
+
 
   $('#clear').click(function () {
     $('#box-container').empty(); // Clears all boxes from the container
@@ -242,6 +326,59 @@ $(function () {
 
     reader.readAsText(file);
   });
+
+  // Hover event to show box info only if EditMode is unchecked
+  $('#box-container').on('mouseenter', '.resizable-box', function () {
+    if (!$('#editModeCheck').is(':checked')) {
+      var label = $(this).find('.box-label').text();
+      var remarks = $(this).data('remarks') || 'No remarks';
+
+      // Set the content of the modal
+      $('#boxInfoLabel').text(label);
+      $('#boxRemarksContent').text(remarks);
+
+      // Show the modal
+      $('#boxInfo').modal('show');
+    }
+  });
+
+  // Mouse leave event to hide box info only if EditMode is unchecked
+  $('#box-container').on('mouseleave', '.resizable-box', function () {
+    if (!$('#editModeCheck').is(':checked')) {
+      // Hide the modal when the mouse leaves the box
+      $('#boxInfo').modal('hide');
+    }
+  });
+
+  // Right-click event on a box
+  $('#box-container').on('contextmenu', '.resizable-box', function(e) {
+    e.preventDefault(); // Prevent the default context menu
+
+    // Set the position of the custom context menu
+    $('#boxContextMenu').css({
+        top: e.pageY + 'px',
+        left: e.pageX + 'px',
+        display: 'block'
+    });
+
+    // Store the ID of the clicked box
+    $('#boxContextMenu').data('targetBoxId', $(this).attr('id'));
+});
+
+// Click event on the Edit option in the context menu
+$('#editBox').click(function() {
+    var targetBoxId = $('#boxContextMenu').data('targetBoxId');
+    if (targetBoxId) {
+        // Trigger the click event on the target box to open the edit modal
+        $('#' + targetBoxId).trigger('click');
+    }
+    $('#boxContextMenu').hide(); // Hide the context menu
+});
+
+// Hide context menu when clicking elsewhere
+$(document).click(function() {
+    $('#boxContextMenu').hide();
+});
 
   //loadBoxesFromJson(myjson);
 
